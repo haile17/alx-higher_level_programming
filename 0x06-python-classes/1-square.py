c6b23a5f7939d87e467781c6e9830a6d88555972
haile17 <97680343+haile17@users.noreply.github.com>
2022-06-21 16:31:06 +0300

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module square
+"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """ Sets private attribute size for square
+        
+        Args:
+            size: size of side of square
+        """
+        self.__size = size 
