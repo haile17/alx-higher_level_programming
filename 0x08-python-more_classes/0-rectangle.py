97ac48a472e7966472a1977f626d4fa7b4de6e4a
haile17 <97680343+haile17@users.noreply.github.com>
2022-06-28 09:12:10 +0300

    python more classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,9 +5,3 @@
 class Rectangle:
     """Represent a rectangle."""
     pass
-
-Rectangle = __import__('0-rectangle').Rectangle
-
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
