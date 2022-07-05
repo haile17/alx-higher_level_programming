6e2457403b1d9ccfb234af7d4537f35abdd42ccd
haile17 <97680343+haile17@users.noreply.github.com>
2022-07-05 11:00:17 +0300

    python

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """
-This program use the inherit for create Rectangle
+Contains the class BaseGeometry and subclass Rectangle
 """
-
-
-from pickle import HIGHEST_PROTOCOL
-
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Class Rectangle based in BaseGeometry
-    """
-
+    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """Constructor of Retangle"""
-        BaseGeometry.integer_validator(self, 'width', width)
+        """instantiation of the rectangle"""
+        self.integer_validator("width", width)
         self.__width = width
-        BaseGeometry.integer_validator(self, 'height', height)
+        self.integer_validator("height", height)
         self.__height = height
